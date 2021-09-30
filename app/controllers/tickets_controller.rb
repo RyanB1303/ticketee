9af054623b0231d6ca6c7fa6844022e53240f28a
Ryan B <ryan.brilliant.nirwana@gmail.com>
2021-09-30 13:48:19 +0700

    implement deleting ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -23,12 +23,18 @@ class TicketsController < ApplicationController
 
   def update
     if @ticket.update(ticket_params)
-      flash[:notice] = "Ticket has been updated."
+      flash[:notice] = 'Ticket has been updated.'
       redirect_to [@project, @ticket]
     else
-      flash.now[:alert] = "Ticket has not been updated."
-      render "edit"
+      flash.now[:alert] = 'Ticket has not been updated.'
+      render 'edit'
     end
+  end
+
+  def destroy
+    @ticket.destroy
+    flash[:notice] = 'Ticket has been deleted.'
+    redirect_to @project
   end
 
   private
