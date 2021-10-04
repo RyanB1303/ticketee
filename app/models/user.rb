3841d92310daece78da898c887cba69ab9b6d96b
RyanB@Manjaro <ryan.brilliant.nirwana@gmail.com>
2021-10-04 18:44:05 +0700

    feature archiveing user, including protection self archiving

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  scope :active, -> { where(:archived_at => nil)}
+  # Ex:- scope :active, -> {where(:active => true)}
   def to_s
     "#{email} (#{admin? ? "Admin" : "User"})"
   end
+  
+  def archive!
+    self.update(archived_at: Time.now)
+  end
+  
+  
 end
